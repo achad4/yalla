96315b6468dbfdce88718b5a97d5d0f18bb1ea34
Avi Chad-Friedman <ajc2212@columbia.edu>
2015-01-03 23:35:29 -0500

    Fixed loading problems with login

--- sling/QuestionFeedViewController.swift
+++ sling/QuestionFeedViewController.swift
@@ -77,8 +77,10 @@ class QuestionFeedTableView : UITableViewController, UITableViewDelegate, UITabl
                     (user:PFUser!, error:NSError!)->Void in
                     if ((user) != nil){
                         println("Login successfull")
+                        self.viewDidLoad()
                     }else{
                         println("Login failed")
+                        self.viewDidAppear(true)
                     }
                     
                 }
@@ -102,6 +104,7 @@ class QuestionFeedTableView : UITableViewController, UITableViewDelegate, UITabl
                     (success:Bool!, error:NSError!)->Void in
                     if !(error != nil){
                         println("Sign Up successfull")
+                        self.tableView.reloadData()
                     }else{
                         let errorString = error.userInfo!["error"] as NSString
                         println(errorString)
@@ -132,7 +135,7 @@ class QuestionFeedTableView : UITableViewController, UITableViewDelegate, UITabl
         return 1
     }
     func loadData(order: Int){
-        
+        self.timeLineData.removeAllObjects()
         //var currentUserData:UserData = UserData(theUser: PFUser.currentUser())
        
         //var findTimeLineData:PFQuery = PFQuery(className: "Message")
@@ -148,7 +151,7 @@ class QuestionFeedTableView : UITableViewController, UITableViewDelegate, UITabl
         
         // Filtering questions to only see those posted by current user
         var currentUser = PFUser.currentUser();
-        
+        println(currentUser.objectForKey("username"))
         // Option to limit number of results from query (if needed)
         //findTimeLineData.limit = 3
         
@@ -160,6 +163,7 @@ class QuestionFeedTableView : UITableViewController, UITableViewDelegate, UITabl
                 for object in objects{
                     let pdf = object as PFObject
                     self.timeLineData.addObject(pdf)
+                    pdf.deleteInBackgroundWithTarget(nil, selector: nil)
                 }
                 //let array:NSArray = self.timeLineData.reverseObjectEnumerator().allObjects
                 //self.timeLineData = array as NSMutableArray
