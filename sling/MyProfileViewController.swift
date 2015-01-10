c3583c4e4310d00d8d03bf2c878070cf118740c0
Evan O'Connor <eco2116@columbia.edu>
2015-01-10 00:38:27 -0500

    Added FB linkage and early stage profile page (note: FB linking currently has URL Scheme error)

---
+++ sling/MyProfileViewController.swift
@@ -0,0 +1,79 @@
+//
+//  MyProfileViewController.swift
+//  sling
+//
+//  Created by Evan O'Connor on 1/9/15.
+//  Copyright (c) 2015 Avi Chad-Friedman. All rights reserved.
+//
+
+import Foundation
+
+class MyProfileViewController : UIViewController {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userPhoto: UIImageView!
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        var myUser = PFUser.currentUser()
+        self.usernameLabel.text = myUser.username
+        
+     
+        FBRequestConnection.startWithGraphPath("me?fields=id,name,picture", completionHandler: {(connection: FBRequestConnection!, result: AnyObject!, error: NSError!) -> Void in
+            if (result? != nil) {
+                NSLog("error = \(error)")
+                var resultdict = result as? NSDictionary
+                
+                // Populate profile page with user's Facebook name
+                if let name = resultdict?["name"] as? String {
+                    self.usernameLabel.text = name
+                }
+                
+                // Populate profile page image view with user's FB profile pic
+                if let picture = resultdict?["picture"] as? NSDictionary {
+                    if let data = picture["data"] as? NSDictionary {
+                        if let photoURL = data["url"] as? String {
+                            let url = NSURL(string: photoURL)
+                            if let imageData = NSData(contentsOfURL: url!) {
+                                self.userPhoto.image = UIImage(data: imageData)
+                            }
+                        }
+                    }
+                }
+                
+                
+                println(result)
+            }
+        } as FBRequestHandler)
+        
+        
+        
+    }
+    
+    @IBAction func linkFBTapped(sender: UIButton) {
+        var user = PFUser.currentUser()
+        if !PFFacebookUtils.isLinkedWithUser(user) {
+                PFFacebookUtils.linkUser(user, permissions:nil, {
+                (succeeded: Bool, error: NSError!) -> Void in
+            if (succeeded) {
+                NSLog("user logged in with Facebook!")
+            }
+            })
+        } else {
+            var alertView:UIAlertView = UIAlertView()
+            alertView.title = "Failed to Link Facebook!"
+            alertView.message = "Your account is already linked"
+            alertView.delegate = self
+            alertView.addButtonWithTitle("OK")
+            alertView.show()
+
+            
+        }
+
+
+    }
+    
+}
+
+
