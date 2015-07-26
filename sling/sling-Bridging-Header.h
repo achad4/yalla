0e765d6b64c5f419c53aa8e00d4fc62708b9d85d
Avi Chad-Friedman <achadfriedman@groupon.com>
2015-07-25 18:47:55 -0700

    got it working with pods

---
+++ sling/sling-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#import <UIKit/UIKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+#import <Parse/Parse.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+//#import <ParseUI/ParseUI.h>
+#import "JSQMessages.h"
+#import "JSQMessageData.h"
+#import "SWRevealViewController.h"
+#import <Bolts/Bolts.h>
